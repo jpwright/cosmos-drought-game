f64623ae3328f84d3faaf1e3647904d1641dc08c
Jason Wright <jpwright0@gmail.com>
2015-07-17 17:16:31 +0000

    fixing

--- server.js
+++ server.js
@@ -20,34 +20,34 @@ io.on('connection', function(socket) {
 	console.log('user connected (id '+socket.id+')');
 	var color = (Math.round(Math.random()*(NUM_COLORS-1))+1);
 	socket.emit('your-info', {id: socket.id, color: color});
-	
+
 	for (var id in players) {
 		socket.emit('existing-player', {id: id, name: players[id].name, color: players[id].color, position: players[id].position});
 	}
-	
+
 	for (var i = 0; i < green_plants.length; i++) {
 		socket.emit('green-plant', {position: green_plants[i]});
 	}
-	     
+
 	players[socket.id] = {socket: socket, name: 'Guest', color: color, position: {x: 135, y: 135}};
-	
+
 	io.emit('new-player', {id: socket.id, name: 'Guest', color: color, position: {x: 135, y: 135}});
 	socket.on('key', function(msg) {
 		//console.log(socket.id + ' pressed ' + msg['dir']);
 		players[socket.id].position = msg.position;
 		io.emit('player-move', {id: socket.id, dir: msg['dir'], delta: msg['delta']});
 	});
-	
+
 	socket.on('green-plant', function(msg) {
 		io.emit('green-plant', {position: msg.position});
 		green_plants.push(msg.position);
 	});
-	
+
 	socket.on('username', function(msg) {
 		players[socket.id].name = msg.name;
 		io.emit('username', {id: socket.id, name: msg.name});
 	});
-	
+
 	socket.on('disconnect', function() {
 		console.log('user disconnected (id '+socket.id+')');
 		io.emit('player-left', {id: socket.id});
@@ -62,7 +62,7 @@ function brownPlants() {
 	if (plant) {
 		io.emit('brown-plant', {position: plant});
 	}
-	setTimeout(brownPlants, (Math.random()*4000)+1000);	
+	setTimeout(brownPlants, (Math.random()*4000)+1000);
 }
 
 brownPlants();
